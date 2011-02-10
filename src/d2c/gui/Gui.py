05a7c4f992e8432a0e0c1e99ebf7118945311584
Chris Willmore <chris.willmore@yahoo.com>
2011-02-10 15:40:22 +0200

    Initial UI cleanup

--- src/d2c/gui/Gui.py
+++ src/d2c/gui/Gui.py
@@ -10,7 +10,8 @@ app = wx.App()
 
 class Gui(wx.Frame):    
     
-    _ID_LISTBOX = 1000
+    _ID_LISTBOX = 1
+    _ID_ADD_IMAGE = 2
     
     def __init__(self, parent=None, id=-1, title='D2C'):
         wx.Frame.__init__(self, parent, id, title, size=(250, 150))
@@ -26,22 +27,34 @@ class Gui(wx.Frame):
 
         self.Bind(wx.EVT_MENU, self.OnQuit, id=1)
         
-        mainPanel = wx.Panel(self, -1)
-
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         
-        #left panel items
-        
-        
-        self._items = wx.ListBox(mainPanel, self._ID_LISTBOX, wx.DefaultPosition, (170, 130), ["Source Images", "Deployment Descriptors", "Deployments"], wx.LB_SINGLE)
+        #left panel items 
+        self._items = wx.ListBox(self, self._ID_LISTBOX, wx.DefaultPosition, (170, 130), ["Source Images", "Deployment Descriptors", "Deployments"], wx.LB_SINGLE)
         self._items.SetSelection(0)
         self.Bind(wx.EVT_LISTBOX, self.OnSelect, id=self._ID_LISTBOX)
 
+        hbox1.Add(self._items, 0, wx.ALL, 5)
+
+
+        self._sourceImagesPanel = wx.Panel(self, -1)
+        wx.Button(self._sourceImagesPanel, self._ID_ADD_IMAGE, 'Add Image', (150, 130), (110, -1))
+        self.Bind(wx.EVT_BUTTON, self.OnAddImage, id=self._ID_ADD_IMAGE)
+        
+        hbox1.Add(self._sourceImagesPanel, 1, wx.ALL, 5)
+        
+        #self._deploymentDescPanel = wx.Panel(self, -1)
+        
+        self.SetSizer(hbox1)
     
     def OnQuit(self, event):
         self.Close()
 
     def OnSelect(self, event):
         print event
+    
+    def OnAddImage(self, event):
+        print "Add Image"
     
    
 
