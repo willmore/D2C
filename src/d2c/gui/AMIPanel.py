3d1ef56b8ba8e42baa1d5639eb6e01c2d84483e5
Chris Willmore <chris.willmore@yahoo.com>
2011-03-12 09:30:02 +0200

    Added placeholder tooltip when right-clicking AMI in list.

--- src/d2c/gui/AMIPanel.py
+++ src/d2c/gui/AMIPanel.py
@@ -15,7 +15,7 @@ class AMIPanel(wx.Panel):
         
         vbox = wx.BoxSizer(wx.VERTICAL)
  
-        self._list = wx.ListCtrl(self.splitter, -1, style=wx.LC_REPORT, size=(110,300))
+        self._list = wx.ListCtrl(self.splitter, -1, style=wx.LC_REPORT, size=(110,250))
         
         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         hbox1.Add(self.splitter, 1, wx.EXPAND)
@@ -33,8 +33,21 @@ class AMIPanel(wx.Panel):
         
         self.SetSizer(vbox)
         
-        self.Layout()     
-           
+        self.Layout()   
+        
+        self.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnRightDown)
+        
+        self._list.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
+
+    def OnRightDown(self, event):
+        item, flags = self._list.HitTest(event.GetPosition())
+        if flags == wx.NOT_FOUND:
+            event.Skip()
+            return
+        self._list.Select(item)
+        self.PopupMenu(MyPopupMenu('Test'), event.GetPosition())
+         
     def addLogPanel(self, id):
         self._logPanel.addPanel(id, wx.TextCtrl(self._logPanel, -1, size=(100,100), style=wx.TE_MULTILINE )) #style=wx.TE_READONLY
         
@@ -53,5 +66,28 @@ class AMIPanel(wx.Panel):
         for ami in images:
             self._list.Append((ami.amiId,ami.srcImg))
     
+class MyPopupMenu(wx.Menu):
+    def __init__(self, WinName):
+        wx.Menu.__init__(self)
+
+        self.WinName = WinName
+        item = wx.MenuItem(self, wx.NewId(), "Item One")
+        self.AppendItem(item)
+        self.Bind(wx.EVT_MENU, self.OnItem1, item)
+        item = wx.MenuItem(self, wx.NewId(),"Item Two")
+        self.AppendItem(item)
+        self.Bind(wx.EVT_MENU, self.OnItem2, item)
+        item = wx.MenuItem(self, wx.NewId(),"Item Three")
+        self.AppendItem(item)
+        self.Bind(wx.EVT_MENU, self.OnItem3, item)
+
+    def OnItem1(self, event):
+        print "Item One selected in the %s window"%self.WinName
+
+    def OnItem2(self, event):
+        print "Item Two selected in the %s window"%self.WinName
+
+    def OnItem3(self, event):
+        print "Item Three selected in the %s window"%self.WinName
 
 
