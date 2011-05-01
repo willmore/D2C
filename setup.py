481cf64f9c44f14be91290eb03a463372c32dc71
Chris Willmore <chris.willmore@yahoo.com>
2011-05-01 12:07:20 +0300

    Added packages to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
+
+D2C_PKGLIST = ['d2c',
+               'd2c.aws',
+               'd2c.controller',
+               'd2c.data',
+               'd2c.gui',
+               'd2c.model']
+    
+
 setup(name='d2c',
       version='1.0',
-      packages=['d2c'],
+      packages=D2C_PKGLIST
       )
