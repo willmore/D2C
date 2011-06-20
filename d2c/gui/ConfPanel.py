1f8f4c8ec097ddc98478e0662c26a4fb6008d051
Cuneyt Sina Koca <sinakoca@gmail.com>
2011-06-20 12:08:27 +0300

    Cosmetic Changes - Title - Size - Allign Save button to right

--- d2c/gui/ConfPanel.py
+++ d2c/gui/ConfPanel.py
@@ -11,12 +11,15 @@ class ConfPanel(wx.Dialog):
     def __init__(self, *args, **kwargs):
         wx.Dialog.__init__(self, *args, **kwargs)
  
+        self.Title = "Amazon Configuration Panel"
+        self.Size = (400,170)
+ 
         self.aws_user_id = wx.TextCtrl(self);
         self._aws_key_id = wx.TextCtrl(self);
         self._aws_secret_access_key = wx.TextCtrl(self);
         self._ec2_cert = wx.TextCtrl(self);
         self._ec2_private_key = wx.TextCtrl(self);   
-        self._updateButton = wx.Button(self, wx.ID_ANY, 'Save Credentials', size=(130, -1))
+        self._updateButton = wx.Button(self, wx.ID_ANY, 'Save Credentials')
         
         
         fgs = wx.FlexGridSizer(7,2,0,0)
@@ -36,7 +39,8 @@ class ConfPanel(wx.Dialog):
                            
                            (wx.StaticText(self, -1, 'EC2 Private Key'),0, wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT|wx.ALL, 2),
                            (self._ec2_private_key,0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL|wx.EXPAND|wx.ALL, 2),
-                            
+                          
+                           (wx.StaticText(self), wx.EXPAND), 
                            (self._updateButton, 1, wx.ALIGN_RIGHT|wx.ALL, 5)])
         
         self.SetSizer(fgs)
