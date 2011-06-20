00ce2dce2b34cf75b6b5c2ff0aeaaa0e34fcba89
Cuneyt Sina Koca <sinakoca@gmail.com>
2011-06-20 10:11:40 +0300

    Centers the GUI

--- d2c/gui/Gui.py
+++ d2c/gui/Gui.py
@@ -28,6 +28,8 @@ class Gui(wx.Frame):
     def __init__(self, parent=None, id=-1, title='D2C'):
         wx.Frame.__init__(self, parent, id, title, size=(750, 450))
 
+	self.Center()
+
         self.__initMenuBar()
         
         vbox = wx.BoxSizer(wx.VERTICAL)
