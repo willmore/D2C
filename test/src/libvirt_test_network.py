c6900725d6f1dbc1ba461879d71f05b284d58921
sinakoca <sinakoca@gmail.com>
2011-07-20 18:04:19 +0300

    gh-8

--- test/src/libvirt_test_network.py
+++ test/src/libvirt_test_network.py
@@ -1,10 +1,12 @@
 import libvirt
 import time
 import subprocess
+import generate_domain_xml
 from d2c.RemoteShellExecutor import RemoteShellExecutor
+import pkg_resources
 
-network_xml_file = "/home/sina/.d2c/mynetwork.xml"
-domain_xml_file = "/home/sina/.d2c/mydomain.xml"
+domain_xml_file  = generate_domain_xml.GenerateXML.generateXML('/home/sina/VirtualBox VMs/ubuntu1004/ubuntu1004.vdi',1,524288)
+network_xml_file = pkg_resources.resource_filename("test", "virtualbox_xml/mynetwork.xml")
 
 def return_xml(xml_location):
     lines = open(xml_location)
@@ -38,7 +40,7 @@ try:
     dom = conn.lookupByName('ubuntu10')
 except:
     print 'No deployment found with that name,creating the one from xml!'
-    dom = libvirt.virConnect.defineXML(conn, return_xml(domain_xml_file))
+    dom = libvirt.virConnect.defineXML(conn, domain_xml_file)
     
 dom.create()
 
@@ -55,8 +57,26 @@ shell_executor.run("pwd")
 
 time.sleep(5)
 
-print "will shut down now"
+print "Will destroy domain now"
 
-dom.shutdown()
+dom.destroy()
+
+print "Domain destroyed, will sleep for 5 seconds"
+
+time.sleep(5)
+
+dom.undefine()
+
+print "Domain undefined"
+
+network.destroy()
+
+print "Network destroyed"
+
+print "Will sleep 5 seconds"
+
+time.sleep(5)
 
 network.undefine()
+
+print "Network undefined"
