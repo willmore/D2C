1cf9ee98fcbd1d5a30b8021b7ba3af8fedb999e4
sinakoca <sinakoca@gmail.com>
2011-07-20 18:04:06 +0300

    gh-8

--- test/src/generate_domain_xml.py
+++ test/src/generate_domain_xml.py
@@ -1,15 +1,27 @@
 from xml.dom.minidom import parse
 import os
 import sys
+import random
+import pkg_resources
 
 class GenerateXML:
-    
-    def generateXML(source_xml_file,generated_xml_file,vdi_image,noOfCPU,memory):
+    @staticmethod   
+    def generateXML(vdi_image,noOfCPU,memory):
+
+        source_xml =  pkg_resources.resource_filename("test", "virtualbox_xml/mydomain.xml")
+        
+        def randomizeName():
+            s="abcdef123"
+            return ''.join(random.sample(s,len(s)))
         
         # set path of vdi image
-        doc = parse(source_xml_file)
+        doc = parse(source_xml)
         ref = doc.getElementsByTagName('source')[0]
         ref.attributes["file"]=vdi_image
+        
+        # set domain name
+        node = doc.getElementsByTagName('name')
+        node[0].firstChild.nodeValue = randomizeName()
         
         # set cpu number
         node = doc.getElementsByTagName('vcpu')
@@ -24,11 +36,14 @@ class GenerateXML:
         node = doc.getElementsByTagName('currentMemory')
         node[0].firstChild.nodeValue = memory
         
-        # persist changes to new file
-        xml_file = open(generated_xml_file, "w")
-        doc.writexml(xml_file, encoding="utf-8")
-        xml_file.close()
+        ## persist changes to new file
+        #xml_file = open(generated_xml_file, "w")
+        #doc.writexml(xml_file, encoding="utf-8")
+        #xml_file.close()
+        print "generated xml file:"
+        print doc.toxml()
         
-        return
+        return doc.toxml()
     
-    generateXML(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
+    
+    #print generateXML(sys.argv[1],sys.argv[2],sys.argv[3])
